4dc1ee79965119248b49630ce8c457b0e2ae96fb
Ilya_Pryzhykau <tuarone@gmail.com>
2024-04-14 17:37:35 +0200

    first review 1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,22 +12,28 @@ def index(request):
         pub_date__lte=timezone.now(),
         category__is_published=True
     ).order_by('-pub_date')
+
     context = {'post_list': post_list[:5]}
     return render(request, template, context)
 
 
 def post_detail(request, post_id: int):
     template = 'blog/detail.html'
-    post = get_object_or_404(Post, pk=post_id, is_published=True, pub_date__lte=timezone.now())
+
+    post = get_object_or_404(
+        Post, pk=post_id, is_published=True, pub_date__lte=timezone.now())
     if not post.category.is_published:
         raise Http404("Категория не найдена")
+
     context = {'post': post}
     return render(request, template, context)
 
 
-def category_posts(request, category_slug: str):
+def category_posts(request, category_slug: str)
     template = 'blog/category.html'
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    category = get_object_or_404(
+        Category, slug=category_slug, is_published=True)
+
     post_list = Post.objects.filter(
         category=category,
         is_published=True,
