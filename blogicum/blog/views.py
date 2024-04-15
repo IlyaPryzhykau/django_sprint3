2f9f0ed3a1d0b191d219a903ac9407bc7387222a
Ilya_Pryzhykau <tuarone@gmail.com>
2024-04-15 08:20:45 +0200

    third review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,41 +5,50 @@ from .models import Post, Category
 
 
 MAX_POSTS = 5
-POST_LIST = Post.objects.filter(
-    is_published=True,
-    category__is_published=True
-)
+
+
+def get_published_posts(category=None):
+    queryset = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+    )
+    if category:
+        queryset = queryset.filter(category=category)
+    else:
+        queryset = queryset.filter(category__is_published=True)
+    return queryset
 
 
 def index(request):
     template = 'blog/index.html'
-    post_list = POST_LIST.filter(
-        pub_date__lte=timezone.now(),
-    )
-
-    context = {'post_list': post_list[:MAX_POSTS]}
+    post_list = get_published_posts()
+    context = {
+        'post_list': post_list[:MAX_POSTS]
+    }
     return render(request, template, context)
 
 
 def post_detail(request, post_id: int):
     template = 'blog/detail.html'
-
     post = get_object_or_404(
-        POST_LIST.filter(pub_date__lte=timezone.now()),
+        get_published_posts(),
         pk=post_id
     )
-
-    context = {'post': post}
+    context = {
+        'post': post
+    }
     return render(request, template, context)
 
 
 def category_posts(request, category_slug: str):
     template = 'blog/category.html'
     category = get_object_or_404(
-        Category, slug=category_slug, is_published=True)
-    post_list = POST_LIST.filter(
-        category=category,
-        pub_date__lte=timezone.now()
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = get_published_posts(
+        category=category
     )
     context = {
         'category': category,
